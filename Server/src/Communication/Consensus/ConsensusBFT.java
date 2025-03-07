9bfed0e88f0e6cb5048570e3dd28456404180c60
A48254-JoseMenezes <jmgilmenezes17@gmail.com>
2025-03-07 01:20:15 +0000

    ideas for byzantine implementation

---
+++ Server/src/Communication/Consensus/ConsensusBFT.java
@@ -0,0 +1,38 @@
+package Communication.Consensus;
+
+public class ConsensusBFT {
+
+    // last message
+    // messages written
+
+
+
+
+    public void sendReadRequest(){
+
+    }
+
+    public void sendStateAnswer(){
+
+    }
+
+    public void sendCollectedAnswers(){
+
+    }
+
+
+    public void sendWriteRequest(){
+
+    }
+
+    public void processQuorumWriteMessages(){
+
+    }
+
+
+    public void sendAccepts(){
+
+    }
+
+
+}
