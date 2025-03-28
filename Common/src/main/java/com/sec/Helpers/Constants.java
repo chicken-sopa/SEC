665717ad182752f64f41709dc68cb914e864fb5a
AntónioBrás <antonio.t.bras@tecnico.ulisboa.pt>
2025-03-28 23:36:34 +0000

    Added JSON and JSON fetch logic

--- Common/src/main/java/com/sec/Helpers/Constants.java
+++ Common/src/main/java/com/sec/Helpers/Constants.java
@@ -23,8 +23,8 @@ public final class Constants {
     }
 
     // BlockChain
-    public static String BlackListContractAddress = "";
-    public static String ISTCoinContractAddress = "";
+    public static final String BlackListContractAddress = "";
+    public static final String ISTCoinContractAddress = "";
         // BlackList
     public static final String addToBlacklistFunctionSignature = "addToBlacklist(address)";
     public static final String removeFromBlacklistFunctionSignature = "removeFromBlacklist(address)";
@@ -36,12 +36,14 @@ public final class Constants {
     public static final String approveFunctionSignature = "approve(address,uint256)";
     public static final String myBalanceFunctionSignature = "myBalance()";
         // Account addresses
-    public static String owner = "0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB";
-    public static String IstCoin = "0x17F6AD8Ef982297579C203069C1DbfFE4348c372";
-    public static String UserA = "0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c";
-    public static String UserB = "0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB";
-    public static String UserC = "0x583031D1113aD414F02576BD6afaBfb302140225";
-    public static String[] allAddressStrings = {owner, IstCoin, UserA, UserB, UserC};
+    public static final String owner = "0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB";
+    public static final String IstCoin = "0x17F6AD8Ef982297579C203069C1DbfFE4348c372";
+    public static final String UserA = "0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c";
+    public static final String UserB = "0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB";
+    public static final String UserC = "0x583031D1113aD414F02576BD6afaBfb302140225";
+    public static final String[] allAddressStrings = {owner, IstCoin, UserA, UserB, UserC};
+        // Genesis
+    public static final String genesisLocation = "EVM/Genesis/Genesis.json";
 
 
 }
--- Server/src/EVM/Genesis/GenesisBlock.java
+++ Server/src/EVM/Genesis/GenesisBlock.java
@@ -2,8 +2,12 @@ package EVM.Genesis;
 
 import com.google.gson.Gson;
 import com.sec.BlockChain.Transaction;
+import com.sec.Helpers.Constants;
 
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import java.util.Scanner;
 
 public class GenesisBlock {
     public String blockHash;
@@ -35,14 +39,28 @@ public class GenesisBlock {
     }
 
     public static GenesisBlock readGenesisBlockFromJson(){
+        String json = FetchJson();
         Gson mapper =  new Gson();
-        String json = ""; // TODO -> fetch actual Json
         try {
             return mapper.fromJson(json, GenesisBlock.class);
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("Problema reading the genesis block, null returned");
+            System.out.println("Genesis block written in incorrect format, null returned");
             return null;
         }
+    }
+
+    private static String FetchJson(){
+
+        InputStream inputStream = GenesisBlock.class
+                .getClassLoader()
+                .getResourceAsStream(Constants.genesisLocation);
+        if (inputStream == null) {
+            throw new RuntimeException("File not found: Genesis.json");
+        }
+
+        return new Scanner(inputStream, StandardCharsets.UTF_8)
+                .useDelimiter("\\A")
+                .next();
     }
 }
