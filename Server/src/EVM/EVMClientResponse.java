1a25b66d941d29429797f5aaec5d5cce255aed71
chicken_soup <54046168+chicken-sopa@users.noreply.github.com>
2025-03-29 01:15:32 +0000

    created response to client from evm

---
+++ Server/src/EVM/EVMClientResponse.java
@@ -0,0 +1,27 @@
+package EVM;
+
+import com.sec.Links.AuthenticatedPerfectLink;
+import com.sec.Messages.BaseMessage;
+import com.sec.Messages.EvmResultMessage;
+
+public class EVMClientResponse implements IEVMClientResponse {
+
+    final int SENDER_ID;
+    final AuthenticatedPerfectLink<BaseMessage> link;
+
+    public EVMClientResponse(int senderId, AuthenticatedPerfectLink<BaseMessage> link) {
+        SENDER_ID = senderId;
+        this.link = link;
+    }
+
+
+    @Override
+    public void sendEVMAnswerToClient(String answerFromEVM) {
+        try {
+            EvmResultMessage message = new EvmResultMessage(this.SENDER_ID, answerFromEVM);
+            link.sendMessage(message, 4550);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- Server/src/module-info.java
+++ Server/src/module-info.java
@@ -3,7 +3,6 @@ module Server {
     requires org.hyperledger.besu.evm;
     requires transitive org.hyperledger.besu.datatypes;
     requires tuweni.bytes;
-    requires com.fasterxml.jackson.databind;
     requires com.google.gson;
     exports Server;
 }
